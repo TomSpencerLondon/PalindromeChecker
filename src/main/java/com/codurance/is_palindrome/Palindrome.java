47f49a57c5db24d8bfd108730bc46f3edc234dcc
Jay Kukadiya <48913659+jaykukadiya@users.noreply.github.com>
2020-10-01 22:48:04 +0530

    Update Palindrome.java

--- src/main/java/com/codurance/is_palindrome/Palindrome.java
+++ src/main/java/com/codurance/is_palindrome/Palindrome.java
@@ -1,25 +1,14 @@
-package com.codurance.is_palindrome;
-
+//no need of packages
 public class Palindrome {
-    public boolean check(String phrase) {
-        char[] a = phrase.toCharArray();
-        int aLength = a.length;
-        char[] b = new char[aLength];
-
-        int j = a.length;
-
-        for(int i = 0; i < aLength; i++){
-            b[j - 1] = a[i];
-            j -= 1;
-        }
-
-        String string1 = new String(a);
-        String string2 = new String(b);
-
-        if(string1.equals(string2)){
-            return true;
-        }
-
+    public boolean check(String phrase){
+      String myString = phrase;
+      StringBuffer buffer = new StringBuffer(myString);
+      buffer.reverse();
+      String data = buffer.toString();
+      if(myString.equals(data)){
+         return true;
+      } else {
         return false;
-    }
+      }
+   }
 }
